1a60936f306f00965c77e055e8bfbceafff0885e
Sam Rogers <sjr@samrogers.org>
2018-04-17 00:55:45 -0400

    api works

--- lib/extext/documents.ex
+++ lib/extext/documents.ex
@@ -1,25 +1,22 @@
 defmodule Extext.Documents do
   @moduledoc false
   def scan(root) do
-    glob = root <> "/**/*.md"
+    glob = root <> "/**/*.{md,MD,Md}"
     Path.wildcard(glob)
-#    |> Path.split
   end
 
+  @doc """
+  scan default dirs as configured
+  """
   def scan() do
     Application.get_env(:extext, :directories)
-    |> Enum.map(&scan/1)
+    |> Enum.flat_map(&scan/1)
   end
 
-  def read(filepath) when is_bitstring(filepath) do
+  def read(filepath) do
     case File.exists?(filepath) do
-      true -> {:ok, File.stream!(filepath)}
-      _ -> {:err, "File does not exist"}
+      true -> File.stream!(filepath)
+      _ -> "File does not exist: #{IO.inspect filepath}" # {:err, "File does not exist: #{IO.inspect filepath}"}
     end
   end
-
-  def read(filepath) when is_list(filepath), do: Path.join(filepath) |> read
-#    Path.join(filepath)
-#    |> read
-#  end
 end
--- lib/extext_web/controllers/files_controller.ex
+++ lib/extext_web/controllers/files_controller.ex
@@ -3,32 +3,20 @@ defmodule ExtextWeb.FilesController do
   use ExtextWeb, :controller
 
   def index(conn, _params) do
-    files = Extext.Documents.scan()
+    files = Extext.Documents.scan
     render conn, "index.json", files: files
-  end
-
-  def contents(conn, %{"filepath" => filepath}) do
-    case Extext.Documents.read(filepath) do
-      {:ok, file} -> render conn, "contents.json", file: file
-      _ -> {:err}
-    end
   end
 end
 
 defmodule ExtextWeb.FilesView do
   def render("index.json", %{files: files}) do
-    files
-    |> Enum.map(&Path.split/1)
-  end
-
-  def render("contents.json", %{file: file}) do
-    file |> file_json
+    files |> Enum.map(&file_json/1)
   end
 
   defp file_json(file) do
     %{
-      path: file.path,
-      lines: Enum.to_list(file)
+      path: file,
+      contents: Extext.Documents.read(file) |> Enum.join
     }
   end
 end
