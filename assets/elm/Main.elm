5710751b4a64446adf2ceea4886490ed53c28aae
Sam Rogers <sjr@samrogers.org>
2018-04-18 19:51:06 -0400

    table of contents

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Html
+import Html.Events exposing (onClick)
 import Json.Decode exposing (map2, field, string, list)
 import Http
 import Result exposing (Result(Ok))
@@ -22,7 +23,29 @@ type Msg =
 
 init = ( Model [] [], loadFiles )
 
-view model = Html.div [] [ Html.text (toString model) ]
+-- view
+
+fileEntry: TxtFile -> Html.Html Msg
+fileEntry f =
+--    Html.button [ onClick (Display f.contents) ] [Html.text f.title]
+    Html.button [] [Html.text f.title]
+contentsList files =
+    (List.map (\x -> Html.li [] [fileEntry x]) files)
+
+view model =
+    let
+        tableOfContents = Html.ul [] (contentsList model.available)
+        header = Html.h2 [] [Html.text "Test Header"]
+        displayContainer = Html.div [] [Html.text "Display container"]
+    in
+        Html.div [] [
+            tableOfContents
+            , header
+            , displayContainer
+        ]
+
+-- update
+
 update msg model =
     case msg of
         Populate (Ok txtFiles) -> (Model txtFiles [], Cmd.none)
