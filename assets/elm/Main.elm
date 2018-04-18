849d3f6bf57b87528025f8ce5a2ecb19ce43dbf0
Sam Rogers <sjr@samrogers.org>
2018-04-18 19:29:29 -0400

    elm basic function

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Html
-import Json.Decode
+import Json.Decode exposing (map2, field, string, list)
 import Http
+import Result exposing (Result(Ok))
 
 main = Html.program
     { init = init
@@ -11,17 +12,32 @@ main = Html.program
     , subscriptions = always Sub.none }
 
 type alias Model =
-    { available : List String
+    { available : List TxtFile
     , history : List String
     }
 
-type Msg = Clear | Populate | Display
+type Msg =
+    Populate (Result Http.Error TxtFileList)
+    | Display String
 
-init = ( Model [] [], Cmd.none )
+init = ( Model [] [], loadFiles )
 
 view model = Html.div [] [ Html.text (toString model) ]
 update msg model =
     case msg of
-        Clear -> init
-        Populate -> (Model ["loading"] [], Cmd.none)
-        Display -> init
+        Populate (Ok txtFiles) -> (Model txtFiles [], Cmd.none)
+        Populate (Err e) -> (model, Cmd.none)
+        Display title -> init
+
+-- JSON
+type alias TxtFile =
+    { title: String
+    , contents: String
+    }
+type alias TxtFileList = List TxtFile
+txtFileDecoder = map2 TxtFile (field "title" string) (field "contents" string)
+txtFileListDecoder = list txtFileDecoder
+loadFiles: Cmd Msg
+loadFiles =
+    let result = Http.get "/files" txtFileListDecoder
+    in Http.send Populate result
